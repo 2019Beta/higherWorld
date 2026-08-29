5809e4c911995a170c0668e4ebceaea334574a36
2019Beta <3285118962@qq.com>
2026-08-29 17:26:08 +0800

    init

---
+++ src/client/java/org/devt/higherworld/client/HigherworldClient.java
@@ -0,0 +1,10 @@
+package org.devt.higherworld.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HigherworldClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/devt/higherworld/Higherworld.java
@@ -0,0 +1,10 @@
+package org.devt.higherworld;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Higherworld implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
